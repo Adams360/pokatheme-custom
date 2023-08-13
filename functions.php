aa1efb42531adc8ac715c0bbed6ffd11ce1b9824
Adam Shaul <ashaul@outbrain.com>
2023-08-13 23:05:02 +0300

    relevant files and sass setup on child theme

--- functions.php
+++ functions.php
@@ -1,14 +1,16 @@
 <?php
 
 // =============================================================================
-// Your functions
+// Custom functions
 // =============================================================================
 
 function poka_child_enqueue_styles() {
-	// Your custom styles
-	wp_enqueue_style( 'child-theme-style', get_stylesheet_directory_uri() . '/style.css', array( 'poka-vendor', 'poka-bundle', 'poka-css-vars' ), filemtime( get_stylesheet_directory() . '/style.css' ) );
+    // Existing theme styles
+    wp_enqueue_style( 'child-theme-style', get_stylesheet_directory_uri() . '/style.css', array( 'poka-vendor', 'poka-bundle', 'poka-css-vars' ), filemtime( get_stylesheet_directory() . '/style.css' ) );
+
+    // Custom styles for the Custom- Rviews Listing block
+    wp_enqueue_style( 'custom-reviews-listing-style', get_stylesheet_directory_uri() . '/assets/public/css/styles.css', array( 'poka-vendor', 'poka-bundle', 'poka-css-vars', 'child-theme-style' ), filemtime( get_stylesheet_directory() . '/assets/public/css/custom-reviews-listing.css' ) );
 }
 add_action( 'wp_enqueue_scripts', 'poka_child_enqueue_styles', 10 );
-
 
 
